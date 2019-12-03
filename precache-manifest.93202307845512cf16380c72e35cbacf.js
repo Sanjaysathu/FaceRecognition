323c2ef9df01ee376c2d1f7d463b363de0445351
sanjaysathu <snjsathu@gmail.com>
2019-12-03 10:56:15 +0530

    Updates

---
+++ precache-manifest.93202307845512cf16380c72e35cbacf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506d4e81146b4ab4f57c6ad4d6410547",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "ca66e9bb8f9406b1d7b0",
+    "url": "/FaceRecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b68d734b8be7ccc77971",
+    "url": "/FaceRecognition/static/css/main.70da530b.chunk.css"
+  },
+  {
+    "revision": "ca66e9bb8f9406b1d7b0",
+    "url": "/FaceRecognition/static/js/2.320e8f86.chunk.js"
+  },
+  {
+    "revision": "b68d734b8be7ccc77971",
+    "url": "/FaceRecognition/static/js/main.232039d5.chunk.js"
+  },
+  {
+    "revision": "5cbe391a43ec582698e7",
+    "url": "/FaceRecognition/static/js/runtime-main.591ab999.js"
+  },
+  {
+    "revision": "8846bd05cb08ca524c9e0b871efb6ab8",
+    "url": "/FaceRecognition/static/media/AI.8846bd05.png"
+  }
+]);
